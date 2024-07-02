465e220a796da72f012d4c3dc3a036e3a6bed26e
anbashayan <anbashayan12@gmail.com>
2024-07-02 21:58:49 +0530

    input

---
+++ input1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class input1 {
+    public static void main(String args[]){
+        System.out.println("Enter a number = ");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        System.out.println("Number that you have entered "+a);
+
+    }
+}
---
+++ varibale.java
@@ -0,0 +1,8 @@
+public class varibale {
+    public static void main(String args[]){
+        int container = 12;
+        String school = "UCSC";
+        System.out.println(container);
+        System.out.println(school);
+    }
+}
